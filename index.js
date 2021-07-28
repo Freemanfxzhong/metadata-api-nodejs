d8a3de986fb02e9314fad512fc763b5bbfcaee8a
Freemanfxzhong <freemanfxzhong@aliyun.com>
2021-07-28 23:22:27 +0800

    add meta

--- index.js
+++ index.js
@@ -26,13 +26,56 @@ app.get('/api/token/:token_id', function(req, res) {
   const month = parseInt(bdayParts[0])
   const data = {
     'name': person.name,
-    'attributes': {
-      'birthday': person.birthday,
-      'birth month': monthName(month),
-      'zodiac sign': zodiac(day, month),
-      // 'age': moment().diff(person.birthday, 'years')
-    },
-    'image': `${HOST}/images/${tokenId}.jpeg`
+    'attributes': [
+      {
+        "trait_type": "Base", 
+        "value": "narwhal"
+      }, 
+      {
+        "trait_type": "Eyes", 
+        "value": "sleepy"
+      }, 
+      {
+        "trait_type": "Mouth", 
+        "value": "cute"
+      }, 
+      {
+        "trait_type": "Level", 
+        "value": 4
+      }, 
+      {
+        "trait_type": "Stamina", 
+        "value": 90.2
+      }, 
+      {
+        "trait_type": "Personality", 
+        "value": "Boring"
+      }, 
+      {
+        "display_type": "boost_number", 
+        "trait_type": "Aqua Power", 
+        "value": 10
+      }, 
+      {
+        "display_type": "boost_percentage", 
+        "trait_type": "Stamina Increase", 
+        "value": 5
+      }, 
+      {
+        "display_type": "number", 
+        "trait_type": "Generation", 
+        "value": 1
+      }
+    ], 
+    // {
+    //   //'birthday': person.birthday,
+    //   // 'birth month': monthName(month),
+    //   // 'zodiac sign': zodiac(day, month),
+    //   // 'age': moment().diff(person.birthday, 'years')
+    // },
+    'image': `${HOST}/images/${tokenId}.jpeg`,
+    "description": "This is logo capture of hackademy 2021 - team Fairchild.Renaissance - member "+person.name, 
+    "external_url": "${HOST}/images/${tokenId}.jpeg"
   }
   res.send(data)
 })
